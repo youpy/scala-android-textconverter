a777f79aaa48577e0c9c51b0633985140aa29bd4
youpy <youpy@buycheapviagraonlinenow.com>
2011-03-08 05:48:56 +0900

    add DefaultActivity for clearing cache

---
+++ src/main/scala/Default.scala
@@ -0,0 +1,26 @@
+package com.buycheapviagraonlinenow.senryu
+
+import _root_.android.app.Activity
+import _root_.android.widget.Button
+import _root_.android.view.View
+import _root_.android.os.Bundle
+
+class DefaultActivity extends Activity {
+  private[this] object Constant {
+    var SERVICES_FILENAME = "services.json"
+  }
+
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+
+    var button = new Button(this)
+    button.setText("Clear Cache")
+    button.setOnClickListener(new View.OnClickListener {
+      override def onClick(v:View) {
+        button.setClickable(false)
+        deleteFile(Constant.SERVICES_FILENAME)
+      }
+    })
+    setContentView(button)
+  }
+}
